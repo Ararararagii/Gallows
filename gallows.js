f1243ebe392865cfd07af782da4cb860759e1cc1
kamil.grams <kamil.grams@sohbi.pl>
2025-05-27 07:17:55 +0200

    end

--- gallows.js
+++ gallows.js
@@ -1,5 +1,10 @@
-var answer = "Białe ściany niczym marmur na skałach";
+var answer = "abc";//Białe ściany niczym marmur na skałach
 answer = answer.toUpperCase();
+
+var mistakeCount = 0;
+
+var correct = new Audio("audio/win.mp3");
+var incorrect = new Audio("audio/lose.mp3");
 
 var emptyAnswer = answer.replace(/\S/g, "-");
 
@@ -16,7 +21,7 @@ function start() {
     var div_content = "";
 
     for (const letter of letters) {
-        div_content += '<div class="letter" onclick="check_letter(\'' + letter + '\')">' + letter + '</div>';
+        div_content += '<div class="letter" onclick="check_letter(\'' + letter + '\')" id="' + letter + '">' + letter + '</div>';
         (letters.indexOf(letter) + 1) % 7 == 0 ? div_content += '<div style="clear:both"></div>' : '';
     }
 
@@ -31,11 +36,41 @@ String.prototype.setCharAt = function (index, char) {
 }
 function check_letter(letter) {
 
-    for (const checkingLetter of answer) {
-        if (letter.toUpperCase() == checkingLetter.toUpperCase()) {
-            emptyAnswer = emptyAnswer.setCharAt(answer.indexOf(checkingLetter), letter);
+    var hit = false;
+
+    for (i = 0; i < answer.length; i++) {
+        if (answer.charAt(i) == letter) {
+            emptyAnswer = emptyAnswer.setCharAt(i, letter);
+            hit = true;
         }
     }
 
-    display_answer();
+    if (hit) {
+        correct.play();
+        document.getElementById(letter).style.background = "#003300";
+        document.getElementById(letter).style.color = "#00C000";
+        document.getElementById(letter).style.border = "3px solid #00C000";
+        document.getElementById(letter).style.cursor = "default";
+
+        display_answer();
+    } else {
+        incorrect.play();
+        document.getElementById(letter).style.background = "#330000";
+        document.getElementById(letter).style.color = "#C00000";
+        document.getElementById(letter).style.border = "3px solid #C00000";
+        document.getElementById(letter).style.cursor = "default";
+        document.getElementById(letter).setAttribute("onclick", "");
+
+        mistakeCount++;
+        var image = "img/s" + mistakeCount + ".jpg";
+        document.getElementById("gallows").innerHTML = '<img src="' + image + '"/>';
+    }
+
+    if (answer == emptyAnswer) {
+        document.getElementById("alphabet").innerHTML = "Brawo! Wygrałeś! <br/> Odpowiedź to: <strong>" + answer + '</strong> <br/><br/><span class="reset" onclick="location.reload()">Jeszcze raz?</span>';
+    }
+
+    if(mistakeCount >= 9) {
+        document.getElementById("alphabet").innerHTML = "Przegrałeś! <br/> Odpowiedź to: <strong>" + answer + '</strong> <br/><br/><span class="reset" onclick="location.reload()">Jeszcze raz?</span>';
+    }
 }
